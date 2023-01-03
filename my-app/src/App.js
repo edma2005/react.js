5cabc1f2b37d3097a7536fb11ede11458eb943c7
Edma <edma20052005@yahoo.co.uk>
2023-01-03 20:12:22 +0200

    update

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="container">
+        <p>
+          My name is Edma!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
